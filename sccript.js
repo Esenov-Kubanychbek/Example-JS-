5c0ea9355ac6e9b9b717bcd787a18b08b2b1092a
Esenov-Kubanychbek <kubaesenov031005@gmail.com>
2021-01-16 13:14:12 +0000

    Add example

---
+++ sccript.js
@@ -0,0 +1,12 @@
+let a = +prompt("Какова ширина?")
+let b = +prompt("Какова высота?")
+
+let area = a * b;
+
+console.log(area);
+
+let a = +prompt("Каковы стороны четырехугольника?")
+
+let area = 4 * a;
+
+console.log(area);
